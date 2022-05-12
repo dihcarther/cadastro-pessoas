776ccd448a8b3db7ce410b3f90aba06015d290ad
dihcarther <antonio-dkm@hotmail.com>
2022-05-12 14:50:19 -0300

    commit

---
+++ ConsumindoReact/CadPessoas/Models/Endereco.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CadPessoas.Models
+{
+
+        [Table("Endereco")]
+    public class Endereco
+    {
+     
+  
+            [Key]
+            [Display(Name = "Id")]
+            [Column("Id")]
+            public int Id { get; set; }
+
+
+            [Display(Name = "IdRef")]
+            [Column("IdRef")]
+            public int UsuarioId { get; set; }
+
+
+            [Display(Name = "Longradouro")]
+            [Column("Longradouro")]
+            public string Longradouro { get; set; }
+
+        }
+    }
+
---
+++ ConsumindoReact/CadPessoas/Models/Pessoa.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CadPessoas.Models
+{
+     
+
+
+        [Table("Pessoa")]
+        public class Pessoa
+        {
+
+            [Display(Name = "Id")]
+            [Column("Id")]
+            public int Id { get; set; }
+
+
+            [Display(Name = "Nome")]
+            [Column("Nome")]
+            public string Nome { get; set; }
+
+
+            [Display(Name = "DataNasc")]
+            [Column("DataNasc")]
+            public string DataNasc { get; set; }
+
+
+        }
+}
