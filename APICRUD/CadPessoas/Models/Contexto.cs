dd86504ba558ffb55ece692f55977aec4946b74d
dihcarther <antonio-dkm@hotmail.com>
2022-05-12 14:52:39 -0300

    commit

---
+++ APICRUD/CadPessoas/Models/Contexto.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CadPessoas.Models;
+
+namespace CadPessoas.Models
+{
+    public class Contexto: DbContext
+    {
+
+        public Contexto(DbContextOptions<Contexto> options):base(options) 
+        
+        
+        {
+             
+             
+        }
+
+        public DbSet<Pessoa> Pessoas { get; set; }
+
+        public DbSet<Endereco> Endereco { get; set; }
+
+
+
+    }
+}
